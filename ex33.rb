98e1f1e080991f1c20e37f165212791f744a6198
vagrant <vivien.fan.1224@gmail.com>
2017-06-04 16:49:08 +0000

    inital commit

---
+++ ex33.rb
@@ -0,0 +1,24 @@
+i = 0
+numbers = []
+
+while i < 6
+  puts "At the top i is #{i}"
+  numbers.push i
+
+  i += 1
+  puts "Numbers now: ", numbers
+  puts "At the bottom i is #{i}"
+end
+
+puts "The numbers: "
+
+# remember you can write this 2 other ways?
+numbers.each do |num|
+  puts num
+end
+
+numbers.each {|num| puts num}
+
+for num in numbers
+  puts num
+end
---
+++ example.rb
@@ -0,0 +1,4 @@
+puts "Hello, what's your name?"
+name = gets.chomp
+puts "Welcome to \"Ruby land\", #{name}. Hope your enjoy your stay (and never leave)!"
+
