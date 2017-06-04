891ebf5d50a90f9589fea8abe6d3e7238d6b55a5
vagrant <vivien.fan.1224@gmail.com>
2017-06-04 17:14:37 +0000

    accessor

--- ex_oop.rb
+++ ex_oop.rb
@@ -1,18 +1,44 @@
-module Speak
-  def speak(sound)
-    puts "#{sound}"
+# module Speak
+#   def speak(sound)
+#     puts "#{sound}"
+#   end
+# end
+
+class GoodDog
+  # def name
+  #   @name
+  # end
+
+  # def name=(name)
+  #   @name = name
+  # end
+
+  # same as the following:
+  attr_accessor :name, :height, :weight
+
+  def initialize(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def speak
+    "#{name} says Arf!"
+  end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{self.name} weighs #{self.weight} and is #{self.height} tall."
   end
 end
 
-class GoodDog
-  include Speak
-end
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+puts sparky.info
 
-class HumanBeing
-  include Speak
-end
-
-sparky = GoodDog.new
-sparky.speak("Arf!")        # => Arf!
-bob = HumanBeing.new
-bob.speak("Hello!")         # => Hello!
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info
